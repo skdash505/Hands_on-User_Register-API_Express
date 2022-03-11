f17710229c6b35a7dc72b481c1fc8dc7f5d27e41
Santanu <skdash505@gmail.com>
2022-03-11 17:40:03 +0530

    Asset deleted.

--- assets/backup/router.js/demo.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Demo', content: 'respond with a resource as Demo' });
-});
-
-module.exports = router;
